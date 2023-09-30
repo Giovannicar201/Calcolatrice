b987d7b196515693d7341e5efcd17dad8a485d0b
AngeloAntonioPrisco <a.prisco50@studenti.unisa.it>
2023-09-30 12:01:58 +0200

    Sottrazione Divisione aggiunte nella classe Calcolatrice con relativi Test Case

--- src/test/java/CalcolatriceTest.java
+++ src/test/java/CalcolatriceTest.java
@@ -7,17 +7,32 @@ public class CalcolatriceTest {
     Calcolatrice calcolatrice;
 
     @Before
-    public void setUp(){
-        calcolatrice= new Calcolatrice();
+    public void setUp() {
+        calcolatrice = new Calcolatrice();
     }
 
     @Test
-    public void TestSomma(){
-        assertTrue("somma corretta",calcolatrice.somma(11,1)==12);
+    public void TestSomma() {
+        assertTrue("somma corretta", calcolatrice.somma(11, 1) == 12);
     }
 
     @Test
-    public void TestMoltiplicazione(){
-        assertTrue("moltiplicazione corretta",calcolatrice.moltiplicazione(10,2)==20);
+    public void TestMoltiplicazione() {
+        assertTrue("moltiplicazione corretta", calcolatrice.moltiplicazione(10, 2) == 20);
+    }
+
+    @Test
+    public void TestSottrazione() {
+        assertTrue("somma corretta", calcolatrice.sottrazione(5, 2) == 3);
+    }
+
+    @Test
+    public void TestDivisione() {
+        assertTrue("divisione corretta", calcolatrice.divisione(10, 2) == 5);
+    }
+
+    @Test
+    public void TestDivisionePerZero() {
+        assertTrue("divisione corretta", calcolatrice.divisione(10, 0) == 0);
     }
 }
